8354a7738e7b0330d1a53b5f5b381f523c17438a
rudak aleksandra <alex.rud.seal@gmail.com>
2026-06-29 15:09:21 +0700

    feat: setup react router v6 and custom sidebar with css modules

---
+++ src/pages/Booking.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Booking: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Бронирование ресурсов</h1>
+      <p className="text-gray-600">Здесь будет интерактивная панель поиска и фильтрации.</p>
+    </div>
+  );
+};
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Profile: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Профиль</h1>
+      <p className="text-gray-600">Здесь будет Профиль</p>
+    </div>
+  );
+};
